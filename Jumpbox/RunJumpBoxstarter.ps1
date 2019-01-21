64eaa56aa1ada7e3503a8eabb9afeed31d46daac
John Engelking <john.engelking@live.com>
2019-01-20 22:40:10 -0700

    Updates

--- Jumpbox/RunJumpBoxstarter.ps1
+++ Jumpbox/RunJumpBoxstarter.ps1
@@ -4,6 +4,4 @@ Invoke-WebRequest `
 -UseBasicParsing https://boxstarter.org/bootstrapper.ps1 | `
 Invoke-Expression; `
 get-boxstarter -Force; `
-Install-BoxstarterPackage `
--PackageName https://gist.githubusercontent.com/jcengelking/a1b6c46ccfa1aee26ea903436375faab/raw/54f36a5dd31a2e5f7c56b6054c30ef4d2c4d7062/boxgist-jbox.txt `
--DisableReboots
+Install-BoxstarterPackage -PackageName https://gist.githubusercontent.com/jcengelking/a1b6c46ccfa1aee26ea903436375faab/raw/54f36a5dd31a2e5f7c56b6054c30ef4d2c4d7062/boxgist-jbox.txt -DisableReboots
