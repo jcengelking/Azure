00f8429fedd7c1d6afcf13af4d5aee48eed2e4cd
John Engelking <john.engelking@live.com>
2019-01-23 19:51:04 -0700

    start SSH services automatically

--- Jumpbox/RunChoco.ps1
+++ Jumpbox/RunChoco.ps1
@@ -9,6 +9,8 @@ try {
     cinst openssh -params '"/SSHServerFeature"' -y
     cinst vscode git googlechrome firefox git-credential-winstore poshgit 7zip sysinternals -y
     cinst Boxstarter -y
+    set-service sshd -StartupType Automatic -PassThru | start-service
+    set-service ssh-agent -StartupType Automatic -PassThru | start-service
 }
 catch {
     Write-Output "Welp."
