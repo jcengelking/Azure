172fd46b620ac5d22d75d154b0c00351c4df71eb
John Engelking <john.engelking@live.com>
2019-05-14 13:16:08 -0600

    updating with sample code from Microsoft

--- AzureFileSync/QuickAFS.ps1
+++ AzureFileSync/QuickAFS.ps1
@@ -25,3 +25,40 @@ foreach ($account in $StorageAccounts) {
     Set-AzStorageShareQuota -Context $storageContext -ShareName "$($account)share" -Quota 1024
 }
 
+# Create Azure File Sync service
+<#
+https://github.com/MicrosoftDocs/azure-docs/blob/master/articles/storage/files/storage-sync-files-deployment-guide.md
+https://docs.microsoft.com/en-us/azure/storage/files/storage-sync-files-deployment-guide?tabs=azure-powershell#tabpanel_CeZOj-G++Q-1_azure-powershell
+#>
+storageSyncName = "AFSSyncService"
+$storageSync = New-AzStorageSyncService -ResourceGroupName $RGname -Name $storageSyncName -Location $Central
+
+
+# Download and install the agent, code sourced directly from docs.microsoft.com
+# Gather the OS version
+$osver = [System.Environment]::OSVersion.Version
+
+# Download the appropriate version of the Azure File Sync agent for your OS.
+if ($osver.Equals([System.Version]::new(10, 0, 17763, 0))) {
+    Invoke-WebRequest `
+        -Uri https://aka.ms/afs/agent/Server2019 `
+        -OutFile "StorageSyncAgent.msi"
+} elseif ($osver.Equals([System.Version]::new(10, 0, 14393, 0))) {
+    Invoke-WebRequest `
+        -Uri https://aka.ms/afs/agent/Server2016 `
+        -OutFile "StorageSyncAgent.msi" 
+} elseif ($osver.Equals([System.Version]::new(6, 3, 9600, 0))) {
+    Invoke-WebRequest `
+        -Uri https://aka.ms/afs/agent/Server2012R2 `
+        -OutFile "StorageSyncAgent.msi" 
+} else {
+    throw [System.PlatformNotSupportedException]::new("Azure File Sync is only supported on Windows Server 2012 R2, Windows Server 2016, and Windows Server 2019")
+}
+
+# Install the MSI. Start-Process is used to PowerShell blocks until the operation is complete.
+# Note that the installer currently forces all PowerShell sessions closed - this is a known issue.
+Start-Process -FilePath "StorageSyncAgent.msi" -ArgumentList "/quiet" -Wait
+
+# Note that this cmdlet will need to be run in a new session based on the above comment.
+# You may remove the temp folder containing the MSI and the EXE installer
+Remove-Item -Path ".\StorageSyncAgent.msi" -Recurse -Force
